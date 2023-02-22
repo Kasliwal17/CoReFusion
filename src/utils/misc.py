79484bae564b7147ee538887c7a94405d2514150
Aditya Kasliwal <kasliwaladitya17@gmail.com>
2023-02-22 17:16:31 +0530

    Update misc.py

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+
+# normalizing target image to be compatible with tanh activation function
 def normalize_data(data):
     data *= 2
     data -= 1
@@ -7,3 +10,16 @@ def unnormalize_data(data):
     data += 1
     data /= 2
     return data
+
+# helper function for data visualization
+def visualize(**images):
+    """PLot images in one row."""
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).title())
+        plt.imshow(image)
+    plt.show()
